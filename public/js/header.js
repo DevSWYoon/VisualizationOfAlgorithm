a264e188c8ec6a926e8a3fdfb950f4e045386e3f
SoekWonYoon <aicsay4838@gmail.com>
2023-08-29 20:58:50 +0900

    Menu Toggle 화 완료
    TODO. Menu 에 ease-in-out 구현,
    버튼 위치 조정

--- public/js/header.js
+++ public/js/header.js
@@ -1,8 +1,16 @@
 const headerContainer = document.querySelector('.header-container');
+
 fetch('../header.html')
     .then(res => res.text())
     .then(html => {
         headerContainer.innerHTML = html;
+
+        const menuToggle = document.getElementById("menuToggle");
+        const menu = document.querySelector(".menu");
+
+        menuToggle.addEventListener("click", () => {
+            menu.classList.toggle("active");
+        });
     });
 
 
